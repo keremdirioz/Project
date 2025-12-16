3728007c1ab7fe9fc7ce32dc7aa4856ec2c77417
keremdirioz <keremdirioz74@gmail.com>
2025-12-16 17:54:26 +0300

    commit 3
    finished bestdayofmonth

--- Main.java
+++ Main.java
@@ -12,9 +12,9 @@ public class Main {
     // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
     public static int[][][] allData = new int[MONTHS][DAYS][COMMS];
 
-    public static int getCommodityIndex(String Commodity) {
+    public static int getCommodityIndex(String commodity) {
         for (int i = 0; i < COMMS; i++) {
-            if (commodities[i].equals(Commodity)){
+            if (commodities[i].equals(commodity)){
                 return i;
             }
         }
@@ -22,43 +22,44 @@ public class Main {
     }
 
     public static void loadData() {
-        for(int m=0;m<MONTHS;m++){
+        for (int m = 0; m < MONTHS; m++) {
             String filename = "Data_Files/" + months[m] + ".txt";
 
-            try{
+            try {
                 Scanner sc = new Scanner(new File(filename));
 
-                while (sc.hasNextLine()){
+                while (sc.hasNextLine()) {
                     String line = sc.nextLine();
-                    String[] parts = line.split(".");
+                    String[] parts = line.split(",");
 
-                    if (parts.length==3){
-                        int day = Integer.parseInt(parts[0].trim()) -1;
-                        String commName = parts[1].trim();
-                        int commIndex= getCommodityIndex(commName);
+                    if (parts.length == 3) {
+                        int day = Integer.parseInt(parts[0].trim()) - 1;
+                        String commodityName = parts[1].trim();
+                        int commIndex = getCommodityIndex(commodityName);
                         int profit = Integer.parseInt(parts[2].trim());
 
-                        if(day>=0 && day<DAYS && commIndex!= -1){
+                        if (day >= 0 && day < DAYS && commIndex != -1) {
                             allData[m][day][commIndex] = profit;
                         }
                     }
                 }
                 sc.close();
-            }catch (FileNotFoundException e){
-                // dosya eğer bulunmazsa sessize al
+            } catch (FileNotFoundException e) {
             }
+
         }
     }
+
 
 
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
     public static String mostProfitableCommodityInMonth(int month) {
         if (month < 0 ||  month >= MONTHS) {
-            return  "INVALİD_MONTH";
+            return  "INVALID_MONTH";
         }
         int maxProfit = Integer.MIN_VALUE;
-        String bestCommName = "";
+        String bestCommName = ",";
 
         for(int c=0; c<COMMS; c++){
             int currentCommTotal = 0;
@@ -76,16 +77,54 @@ public class Main {
     }
 
     public static int totalProfitOnDay(int month, int day) {
-        return 1234;
+        if (month < 0 || month>= MONTHS || day<1 || day>DAYS ) {
+            return -99999; // exception almak yasak olduğu için negatif büyük bir sayı aldım bu sayede kar ile karşılaşaştırılmaz.
+        }
+        int totalProfit= 0; // günlük total karı burda biriktiriyorum.
+        for(int c=0; c<COMMS;c++){ // belirli gün için emtiaların dğerlerine bakıyorum
+            totalProfit += allData[month][day - 1][c];
+        }
+        return totalProfit; // tüm emtiaların total karına burda bakıyorum
     }
 
-    public static int commodityProfitInRange(String commodity, int from, int to) {
-        return 1234;
+public static int commodityProfitInRange(String commodity, int from, int to) {
+        int commIndex= getCommodityIndex(commodity); // emtia adını dzideki index numaasına döndür
+    if (commIndex == -1 || from < 1 || to > DAYS || from > to) {
+        return -99999;
     }
 
-    public static int bestDayOfMonth(int month) { 
-        return 1234; 
+    int totalProfit = 0; // total kar değişkeni
+
+    for (int m = 0; m < MONTHS; m++) {
+        for (int d = from - 1; d <= to - 1; d++) { // -1 yapmamızın sebebi aylar 0 dan başlıyor
+            totalProfit += allData[m][d][commIndex]; // belirli ay gün ve ürün için kaarı bulur
+        }
     }
+    return totalProfit;
+    // Belirli bir emtianın gün aralığına göre toplam kârını hesaplayan metot eklendi
+}
+
+
+    public static int bestDayOfMonth(int month) {
+        if (month < 0 || month >= MONTHS) {
+            return -1;
+        }
+
+        int bestDay = 1;
+        int maxProfit = Integer.MIN_VALUE;
+
+        for (int d = 0; d < DAYS; d++) {
+            int dailyProfit = totalProfitOnDay(month, d + 1);
+
+            if (dailyProfit > maxProfit) {
+                maxProfit = dailyProfit;
+                bestDay = d + 1;
+            }
+        }
+
+        return bestDay;
+    }
+
     
     public static String bestMonthForCommodity(String comm) { 
         return "DUMMY"; 
